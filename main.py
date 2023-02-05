c4ce31f64f4b16bb70bfb6784e6c92f8fac738fd
Varvara0804 <118054516+Varvara0804@users.noreply.github.com>
2023-02-05 13:26:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 class Printer:
    def __init__(self, char, width, height):
        self.char = char
-       self.widch = width
+       self.width = width
        self.height = height
-
 
    def set_char(self, x):
        self.char = x
 
-
-
-
    def print(self):
        for i in range(self.height):
-           print(self.char * self.widch)
+           print(self.char * self.width)
 
-   def set_width(self):
-       self.widch = 7
-
+   def set_width(self, k):
+       self.width = k
 
 
 myPrinter = Printer('0', 5, 2)
 myPrinter.print()
 myPrinter.set_char('x')
 myPrinter.print()
-myPrinter.set_width()
+myPrinter.set_width(7)
 myPrinter.print()
-
