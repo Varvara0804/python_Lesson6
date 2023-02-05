93f3afe6a42191dde25a0716bcb677b509bdc63c
Varvara0804 <118054516+Varvara0804@users.noreply.github.com>
2023-02-05 13:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,29 @@
-import random
-import time
+class Printer:
+   def __init__(self, char, width, height):
+       self.char = char
+       self.widch = width
+       self.height = height
+
+
+   def set_char(self, x):
+       self.char = x
 
 
 
 
-class Cat:
-    def __init__(self, name, playfulness, energy, hunger):
-        self.name = name
-        self.playfulness = playfulness
-        self.energy = energy
-        self.hunger = hunger
-        print(f"__________________________________________")
-        print(f"Новое животное - кошка '{self.name}'")
-        print(f"Характер '{self.playfulness}'")
-        print(f"Энергия '{self.energy}'")
-        print(f"Голод '{self.hunger}'")
-        print(f"__________________________________________")
+   def print(self):
+       for i in range(self.height):
+           print(self.char * self.widch)
 
-    def change_playfulness(self, new_playfulness):
-        self.playfulness = new_playfulness
-        print(f"__________________________________________")
-        print(f"Вы играете с кошкой '{self.name}'")
-        print(f"Игра завершена")
-        print(f"Кошка теперь '{self.playfulness}'")
-        print(f"__________________________________________")
-
-
-    def change_hunger(self, new_hunger):
-        self.hunger = new_hunger
-        print(f"__________________________________________")
-        print(f"Вы кормите кошку '{self.name}'")
-        print(f"Кошка наелась")
-        print(f"Голод понизился до '{self.hunger}'")
-        print(f"__________________________________________")
+   def set_width(self):
+       self.widch = 7
 
 
 
-class Dragon:
-    def __init__(self, name, color, movement, aggression):
-        self.name = name
-        self.color = color
-        self.movement = movement
-        self.aggression = aggression
-        print(f"__________________________________________")
-        print(f"прилетело существо - дракон '{self.name}'")
-        print(f"Он имеет цвет '{self.color}'")
-        print(f"Он передвигается '{self.movement}'")
-        print(f"Характер '{self.aggression}'")
-        print(f"__________________________________________")
+myPrinter = Printer('0', 5, 2)
+myPrinter.print()
+myPrinter.set_char('x')
+myPrinter.print()
+myPrinter.set_width()
+myPrinter.print()
 
-    def change_color(self, new_color):
-        self.color = new_color
-        print(f"__________________________________________")
-        print(f"Вы случайно ударили дракона")
-        print(f"Он стал '{self.color}'")
-        print(f"__________________________________________")
-
-
-
-    def aggression_increase(self):
-        print(f"__________________________________________")
-        print(f"Вы разозлили дракона '{self.name}'")
-        while self.aggression < 100:
-            print(f"Злость дракона '{self.name}' ({self.aggression})")
-            self.aggression = self.aggression + random.randrange(15)
-            time.sleep(0.3)
-        print(f"Злость достигла 100%, вы испепелены")
-
-animal1 = Cat("Муся", "игривая", 60, 40)
-animal2 = Dragon("Григорий", "зелёный", "полёт", 20)
-
-
-animal1.change_playfulness("устала")
-animal1.change_hunger(0)
-animal2.change_color("фиолетовый в крапинку")
-animal2.aggression_increase()
